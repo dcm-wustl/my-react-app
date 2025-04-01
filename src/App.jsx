7d267c6a058ada7cc265bc96cdd81b7c7ce7c77c
Dev Mukundan <d.c.mukundan@wustl.edu>
2025-04-01 16:57:46 -0500

    Added function handleClick inside of Square that occurs when the square is clicked to log each click in the console

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,18 @@ export default function App() {
 }
 
 function Square() {
-  return <button className="square">{value}</button>;
+  function handleClick() {
+    console.log('clicked!');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
